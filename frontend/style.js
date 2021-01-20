f40f15accffc381a5f1129ba07672cd2c26741cd
vitorthegreat <vitor.services@gmail.com>
2021-01-20 22:40:36 +0000

    slide da loja, newsletter, vitrine

--- frontend/style.js
+++ frontend/style.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $('.carousel').slick({
-        dots:true
-    });
-    $('.vitrine').slick({
         dots: true,
-        slidesToShow: 4
+        arrows: false
+    });
+    $('.vitrine-carousel').slick({
+        dots: false,
+        slidesToShow: 4,
+        arrows: true
     });
 });
